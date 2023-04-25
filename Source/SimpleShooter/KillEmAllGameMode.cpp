3d853f61c7d4797de75df73ec2fd915bb26bd87e
xinran <wxinran6@vt.edu>
2023-04-25 23:44:25 +0100

    loop through actor controller

--- Source/SimpleShooter/KillEmAllGameMode.cpp
+++ Source/SimpleShooter/KillEmAllGameMode.cpp
@@ -1,6 +1,7 @@
 
 #include "KillEmAllGameMode.h"
-
+#include "EngineUtils.h"
+#include "GameFramework/Controller.h"
 
 void AKillEmAllGameMode::PawnKilled(APawn* PawnKilled)
 {
@@ -11,6 +12,17 @@ void AKillEmAllGameMode::PawnKilled(APawn* PawnKilled)
     APlayerController* PlayerController = Cast<APlayerController>(PawnKilled->GetController());
     if (PlayerController != nullptr)
     {
-        PlayerController->GameHasEnded(nullptr, false);
+        EndGame(false);
+    }
+
+}
+
+void AKillEmAllGameMode::EndGame(bool bIsPlayerWinner)
+{
+    for (AController* Controller : TActorRange<AController>(GetWorld()))
+    {
+        bool bIsWinner = Controller->IsPlayerController() == bIsPlayerWinner;
+        Controller->GameHasEnded(Controller->GetPawn(), bIsWinner);
+        
     }
 }
